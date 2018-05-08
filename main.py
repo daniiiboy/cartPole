93670a3f0968561524c9bfcc33ae3c8c21da8393
daniiiboy <daniyara@hotmail.com>
2018-05-07 22:22:48 -0400

    Tests
    Ran Tests on different bath sizes (1,2,4,8,16), learning rates (0.1,0.5,0.25... n/2(10th)), and discount factor (1,0.995,0.990,0.985...0.95)

--- main.py
+++ main.py
@@ -44,32 +44,36 @@ if __name__ == "__main__":
     ds = []
     perfs = []
     
-    for b in range(5):
-        print('Buffer Size {}'.format(buff))
-        alpha = 0.1
-        for a in range(10):
-            print('   Learning Rate {:.6f}'.format(alpha))
-            d_factor = 1.0
-            for d in range(10):
-                print('      Discount Factor {:.2f}'.format(d_factor))
-                policy = model.SimpleNet(dim_in = 6) 
-                policyCartPole.train(env, policy, noise=True, printUpdate = False, n_episodes=500, save_model= 50)
-                
-                perfs.append(util.evaluateFinalCartPole(env, policy, noise=True))
-                print('                          {:3.2f}'.format(perfs[-1]))
-                
-                save_path = ('performance/simpleNet_b{}_a{:.6f}_df{:.3f}'.format(buff,alpha,d_factor))
-                util.learningCurve(env, policy, noise=True, save_path=save_path, save_model_n=50, n_episodes=500)
-                
-                bs.append(buff)
-                als.append(alpha)
-                ds.append(d_factor)
-                
-                d_factor -=0.005
-            alpha /= 2
-        buff *=2
-        
-    final = np.vstack((bs,als,ds,perfs))
-    np.save('performance/hyperParamTune_episode500_saveModel50', final)
+    for i in range(20):
+        buff = 1
+        print('TIME {}'.format(i))
+        for b in range(5):
+            print('Buffer Size {}'.format(buff))
+            alpha = 0.1
+            for a in range(10):
+                print('   Learning Rate {:.6f}'.format(alpha))
+                d_factor = 1.0
+                for d in range(10):
+                    print('      Discount Factor {:.2f}'.format(d_factor))
+                    policy = model.SimpleNet(dim_in = 6) 
+                    policyCartPole.train(env, policy, noise=True, printUpdate = False, n_episodes=300, save_model= 50)
+                    
+                    perfs.append(util.evaluateFinalCartPole(env, policy, noise=True))
+                    print('                          {:3.2f}'.format(perfs[-1]))
+                    
+                    save_path = ('performance/simpleNet_ave_b{}_a{:.6f}_df{:.3f}'.format(buff,alpha,d_factor))
+                    util.learningCurve(env, policy, noise=True, save_path=save_path, save_model_n=50, n_episodes=300)
+                    
+                    bs.append(buff)
+                    als.append(alpha)
+                    ds.append(d_factor)
+                    
+                    d_factor -=0.005
+                alpha /= 2
+            buff *=2
+            
+        final = np.vstack((bs,als,ds,perfs))
+        path_name = 'performanceAve/hyperParamTune_{}ave_episode300_saveModel50'.format(i)
+        np.save(path_name, final)
     
      
